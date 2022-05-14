0f107ced71d205286eaba1733b6e1cc081f3a17c
IseUtoshi <sugar.buseinss@gmail.com>
2022-05-14 12:39:01 +0900

    出品時のコントローラー設定、未ログイン時の挙動設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,18 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :explain, :price, :category_id, :status_id, :delivery_charge_id, :delivery_from_id, :delivery_duration_id, :image).merge(user_id: current_user.id)
   end
 
 end
