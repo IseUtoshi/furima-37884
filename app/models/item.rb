99af82eafc8f7b970e3ddecceecefaa0b121c3e8
IseUtoshi <sugar.buseinss@gmail.com>
2022-05-14 11:41:37 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,11 +3,11 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :explain, presence: true
   validates :price, presence: true
-  validates :category_id, presence: true
-  validates :status_id, presence: true
-  validates :delivery_charge_id, presence: true
-  validates :delivery_from_id, presence: true
-  validates :delivery_duration_id, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_from_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_duration_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :image, presence: true
 
   belongs_to :user
