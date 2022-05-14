33669386436e64154c1b8d3b8583e6a3e915e01c
IseUtoshi <sugar.buseinss@gmail.com>
2022-05-14 09:51:53 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,15 @@
 class Item < ApplicationRecord
 
+  validates :name, presence: true
+  validates :explain, presence: true
+  validates :price, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :delivery_from_id, presence: true
+  validates :delivery_duration_id, presence: true
+  validates :image, presence: true
+
   belongs_to :user
   has_one_attached :image
 
