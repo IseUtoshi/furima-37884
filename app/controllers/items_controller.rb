06a158a8741e998bdd30e7a2a2f0427d87ba3d81
IseUtoshi <sugar.buseinss@gmail.com>
2022-05-17 16:46:16 +0900

    削除ボタンのリンクとコントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,8 +35,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
   end
-  
+
   private
 
   def find_item
