7fdc8bc645fd404650868bb7ac3335ccdb776a83
IseUtoshi <sugar.buseinss@gmail.com>
2022-05-19 17:56:18 +0900

    テストコード用ファイルの編集

--- spec/factories/purchase_delivery_addresses.rb
+++ spec/factories/purchase_delivery_addresses.rb
@@ -1,15 +1,11 @@
 FactoryBot.define do
   factory :purchase_delivery_address do
-    transient do
-      person { Gimei.name }
-    end
-    postcode         { Faker::Commerce.material }
-    building         { Faker::Commerce.material }
-    house_number     { Faker::Commerce.material }
-    municipality     { Faker::Commerce.material }
+    Faker::Config.locale = 'ja'
+    postcode         { Faker::Address.postcode }
+    building         { Faker::Address.building_number }
+    house_number     { Faker::Address.street_address }
+    municipality     { Faker::Address.city }
     prefecture_id    { Faker::Number.within(range: 2..48) }
-    telephone_number { Faker::Commerce.material }
-    association :user
-    association :item
+    telephone_number { Faker::Number.leading_zero_number(digits: 10) }
   end
 end
--- spec/models/purchase_delivery_address_spec.rb
+++ spec/models/purchase_delivery_address_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseDeliveryAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @purchase_delivery_address = FactoryBot.build(:purchase_delivery_address)
+  end
+
 end
