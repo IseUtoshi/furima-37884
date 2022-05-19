ce77f109b09ffb2c364dbefed65c3a34834f3f95
IseUtoshi <sugar.buseinss@gmail.com>
2022-05-19 12:13:25 +0900

    トークン取得のための記述

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -3,7 +3,20 @@ const pay = () => {
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("test")
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+    const card = {
+      number: formData.get("purchase_delivery_address[number]"),
+      cvc: formData.get("purchase_delivery_address[cvc]"),
+      exp_month: formData.get("purchase_delivery_address[exp_month]"),
+      exp_year: `20${formData.get("purchase_delivery_address[exp_year]")}`,
+    };
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token)
+      }
+    });
   });
 };
 
