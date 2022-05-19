cd9c66ffde87a4bef6d2f018fedcd3a1d98eb9df
IseUtoshi <sugar.buseinss@gmail.com>
2022-05-19 12:35:23 +0900

    カード情報の削除と送信、トークンのマージ

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -18,6 +18,13 @@ const pay = () => {
         const tokenObj =`<input value=${token} name='token' type="hidden">`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
+
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+
+      document.getElementById("charge-form").submit();
     });
   });
 };
