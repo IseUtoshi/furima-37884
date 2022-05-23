a84c55e75285f9413673a7a52cba3d778eedeec0
IseUtoshi <sugar.buseinss@gmail.com>
2022-05-23 12:48:56 +0900

    出品/編集時のエラーメッセージ修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,11 @@ class Item < ApplicationRecord
   validates :price, presence: true,
                     numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than: 10_000_000,
                                     message: 'は300以上1000万未満の半角整数にしてください' }
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :status_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :delivery_from_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :delivery_duration_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id, numericality: { other_than: 1, message: "を入力してください" }
+  validates :status_id, numericality: { other_than: 1, message: "を入力してください" }
+  validates :delivery_charge_id, numericality: { other_than: 1, message: "を入力してください" }
+  validates :delivery_from_id, numericality: { other_than: 1, message: "を入力してください" }
+  validates :delivery_duration_id, numericality: { other_than: 1, message: "を入力してください" }
   validates :image, presence: true
 
   belongs_to :user
